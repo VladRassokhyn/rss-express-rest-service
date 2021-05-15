3048038a8bf9cb1d3528acf1c8caf9827dc85ec3
vladmoskov <perishvlad@gmail.com>
2021-05-15 20:37:54 +0300

    fix: post request - User.oResponse in response

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,9 +1,12 @@
+const { v4: uuidv4 } = require('uuid');
+const User = require('./user.model');
+
 const users = [];
 
 const getAll = async () => users;
 const postUser = async (userData) => {
-  users.push(userData);
-  return userData;
+  users.push(new User({...userData, id: uuidv4()}));
+  return User.toResponse(users[users.length - 1]);
 };
 const getUserById = async (id) => users.find(user => user.id === id);
 const updateUser = async (id, {login, name, password}) => {
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,13 +1,8 @@
-const { v4: uuidv4 } = require('uuid');
 const usersRepo = require('./user.memory.repository');
-const User = require('./user.model');
 const taskService = require("../tasks/task.service");
 
 const getAll = () => usersRepo.getAll();
-const postUser = ({name,login,password}) => {
-  const user = new User({name,login,password, id: uuidv4()})
-  return usersRepo.postUser(user);
-}
+const postUser = (userData) => usersRepo.postUser(userData);
 const getUserById = (id) => usersRepo.getUserById(id)
 const updateUser = (id, userData) => usersRepo.updateUser(id, userData)
 const removeUser = async (id) => {
